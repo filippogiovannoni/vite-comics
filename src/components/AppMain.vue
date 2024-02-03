7930f289ffe3013df2c37b83e88c0cdeaefd2691
filippogiovannoni <filippogiovannonii@gmail.com>
2024-02-03 23:27:22 +0100

    add bonus component for bottom main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,14 @@
 <script>
+
+import AppMainBottom from './AppMainBottom.vue';
+
+
 export default {
     name: 'AppMain',
     data() {
-        return {
-
-        }
-    }
+        return {};
+    },
+    components: { AppMainBottom }
 }
 </script>
 
@@ -16,32 +19,7 @@ export default {
                 <span>Content goes here</span>
             </div>
         </div>
-        <div class="main-bottom">
-            <div class="container">
-                <ul>
-                    <li>
-                        <img src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics">
-                        <span>Digital comics</span>
-                    </li>
-                    <li>
-                        <img src="../assets/img/buy-comics-merchandise.png" alt="digital-comics">
-                        <span>Dc merchandise</span>
-                    </li>
-                    <li>
-                        <img src="../assets/img/buy-comics-shop-locator.png" alt="digital-comics">
-                        <span>Subscription</span>
-                    </li>
-                    <li>
-                        <img src="../assets/img/buy-comics-subscriptions.png" alt="digital-comics">
-                        <span>Comic shop locator</span>
-                    </li>
-                    <li>
-                        <img src="../assets/img/buy-dc-power-visa.svg" alt="digital-comics">
-                        <span>Dc power visa</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <AppMainBottom></AppMainBottom>
     </main>
 </template>
 
@@ -56,39 +34,6 @@ export default {
         color: var(--dc-white);
         align-items: center;
         font-weight: 600;
-    }
-}
-
-.main-bottom {
-    background-color: var(--dc-primary);
-    display: flex;
-    padding: 2rem 0;
-    position: relative;
-    z-index: 1;
-
-    & ul {
-        display: flex;
-        list-style: none;
-        justify-content: center;
-        gap: 3rem;
-        align-items: center;
-
-        & li {
-            display: flex;
-            align-items: center;
-
-            & img {
-                width: 35px;
-            }
-        }
-
-        & span {
-            vertical-align: middle;
-            padding-left: 1rem;
-            color: var(--dc-white);
-            text-transform: uppercase;
-            font-size: 12px;
-        }
     }
 }
 </style>
---
+++ src/components/AppMainBottom.vue
@@ -0,0 +1,75 @@
+<script>
+export default {
+    name: 'AppMainBottom',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="main-bottom">
+        <div class="container">
+            <ul>
+                <li>
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics">
+                    <span>Digital comics</span>
+                </li>
+                <li>
+                    <img src="../assets/img/buy-comics-merchandise.png" alt="digital-comics">
+                    <span>Dc merchandise</span>
+                </li>
+                <li>
+                    <img src="../assets/img/buy-comics-shop-locator.png" alt="digital-comics">
+                    <span>Subscription</span>
+                </li>
+                <li>
+                    <img src="../assets/img/buy-comics-subscriptions.png" alt="digital-comics">
+                    <span>Comic shop locator</span>
+                </li>
+                <li>
+                    <img src="../assets/img/buy-dc-power-visa.svg" alt="digital-comics">
+                    <span>Dc power visa</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.main-bottom {
+    background-color: var(--dc-primary);
+    display: flex;
+    padding: 2rem 0;
+    position: relative;
+    z-index: 1;
+
+    & ul {
+        display: flex;
+        list-style: none;
+        justify-content: center;
+        gap: 3rem;
+        align-items: center;
+
+        & li {
+            display: flex;
+            align-items: center;
+
+            & img {
+                width: 35px;
+            }
+        }
+
+        & span {
+            vertical-align: middle;
+            padding-left: 1rem;
+            color: var(--dc-white);
+            text-transform: uppercase;
+            font-size: 12px;
+        }
+    }
+}
+</style>
